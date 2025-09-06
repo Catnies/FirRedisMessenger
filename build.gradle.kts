030d10f9085e5525ef58d24ce51726c7e354bad7
Catnies <catnies@foxmail.com>
2025-09-06 16:29:54 +0800

    add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("maven-publish") // Maven Publish
 }
 
 group = "top.catnies"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -17,4 +18,24 @@ dependencies {
     implementation("io.lettuce:lettuce-core:6.5.3.RELEASE") // Lettuce
     implementation("com.google.code.gson:gson:2.8.9") // Gson
     implementation("com.google.guava:guava:30.1-jre") // Guava
+}
+
+// 发布到 Maven 仓库
+publishing {
+    repositories {
+        maven {
+            name = "Catnies"
+            url = uri("https://repo.catnies.top/releases")
+            credentials(PasswordCredentials::class)
+            authentication { create<BasicAuthentication>("basic") }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "top.catnies"
+            artifactId = "firredismessenger"
+            version = "${rootProject.version}"
+            from(components["java"])
+        }
+    }
 }
